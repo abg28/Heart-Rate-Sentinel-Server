965eb827b65581395094ec10e856bf26f9834508
Alex Guevara <abg28@duke.edu>
2018-11-25 22:08:53 -0500

    Attempt to fix mongo empty list ValidationError

--- mongoSetup.py
+++ mongoSetup.py
@@ -1,12 +1,12 @@
-from pymodm import connect
-from pymodm import MongoModel, fields
-
-connect("mongodb://abg28:GODUKE10@ds225253.mlab.com:25253/bme590")
+from pymodm import MongoModel, fields, base
 
 
 class Patient(MongoModel):
     ID = fields.FloatField(primary_key=True)
     attending_email = fields.CharField()
     age = fields.FloatField()
-    heart_rates = fields.ListField()
-    timestamps = fields.ListField()
+    heart_rates = fields.ListField(field=base.fields.
+                                   MongoBaseField(blank=True))
+    timestamps = fields.ListField(field=base.fields.
+                                  MongoBaseField(blank=True))
+
