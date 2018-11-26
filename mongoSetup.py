a523e9e4c76b3842f5e483d8554d82bc918c9099
Alex Guevara <abg28@duke.edu>
2018-11-25 23:09:07 -0500

    Fixes? patient ID bug (NOW? int instead of float)

--- mongoSetup.py
+++ mongoSetup.py
@@ -2,7 +2,7 @@ from pymodm import MongoModel, fields, base
 
 
 class Patient(MongoModel):
-    ID = fields.FloatField(primary_key=True)
+    ID = fields.IntegerField(primary_key=True)
     attending_email = fields.CharField()
     age = fields.FloatField()
     heart_rates = fields.ListField()
